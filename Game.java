ad7a59a3498a8e92e94096cca9a53904437f394d
chaneym <chaneym@gmail.com>
2014-03-22 16:23:12 -0700

    added character and moster select

--- Game.java
+++ Game.java
@@ -2,24 +2,70 @@ import java.util.*;
 
 public class Game
 {
+	Random rand = new Random();
+
+	public static final int OGRE     = 1;
+    public static final int GREMLIN  = 2;
+    public static final int SKELETON = 3;
+
+	Sorceress _sorceress;
+	Thief _thief;
 	Warrior _warrior;
 	Ogre _ogre;
+	Gremlin _gremlin;
+	Skeleton _skeleton;
 
 	public Game(Scanner user)
 	{
-
-		
-		//System.out.println("Enter the type of character you want to play as");
-		//System.out.print("")
-		_warrior = new Warrior(user);
-		_ogre = new Ogre(user);
+		choose_hero();
+		choose_enemy(rand);
 	}
 
+	public void choose_hero()
+	{
+		System.out.println("Enter the type of character you want to play as");
+		String char = user.nextLine();
+
+		if ( char == equalsIgnoreCase(Warrior) )
+			{
+				Warrior _warrior = new Warrior(user);
+			}
+		else if ( char == equalsIgnoreCase(Thief) )
+			{
+				Thief _thief = new Thief(user);
+			}
+		else if ( char == equalsIgnoreCase(Sorceress) )
+			{
+				Sorceress _sorceress = new Sorceress(user);
+			}
+		else choose_hero();
+	}
+
+	public void choose_enemy()
+	{
+	int rand_enemy = (Math.abs(rand.nextInt() ) % 4;
+
+		if ( rand_enemy == OGRE )
+			{
+			Ogre _ogre = new _ogre(user);
+			}
+		else if ( rand_enemy == GREMLIN )
+			{
+			Gremlin	_gremlin = new Gremlin(user);
+			}
+		else ( enemy == SKELETON )
+			{
+			Skeleton _skeleton = new Skeleton(user);
+			}
+	}
+	
 	public void play_round(Scanner user)
 	{
 		int round = 1;
 		boolean done = false;
 		//methods may need to be redefined in their subclasses
+
+		// hero/ogre here needs to be (hero) (monster)
 
 		while( !done && _warrior.is_alive() && _ogre.is_alive() )
 		{
